15f525f639eecdb5aa6394726a69d283724974df
Chappydev <danjtchapman@gmail.com>
2022-09-24 16:45:59 -0600

    Complete 5.9

--- src/App.js
+++ src/App.js
@@ -154,7 +154,7 @@ const App = () => {
       <Notification isError={isError} message={notificationMessage} />
       <p>{user.name} is logged in</p>
       <button onClick={handleLogout}>Logout</button>
-      {blogs.map(blog =>
+      {blogs.sort((a, b) => b.likes - a.likes).map(blog =>
         <Blog key={blog.id} blog={blog} addLike={addLike} />
       )}
       <Togglable buttonLabel="New Note" ref={newBlogFormRef}>
