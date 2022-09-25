244e57bdf33c131581c4101465eefa2ccb816dd8
Chappydev <danjtchapman@gmail.com>
2022-09-25 16:46:14 -0600

    Complete 5.14-5.15

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -27,12 +27,34 @@ describe('<Blog />', () => {
   });
 
   test('renders only the title and author by default', () => {
-    const url = screen.queryByText('www.website...');
+    const url = screen.queryByText(blog.url);
     const likes = screen.queryByText(`likes: ${blog.likes} `);
     expect(url).toBeNull();
     expect(likes).toBeNull();
-    
+
     const titleAndAuthor = screen.getByText('Title... Author...');
     expect(titleAndAuthor).toBeDefined();
+  });
+
+  test('shows url and # of likes after view button is clicked', async () => {
+    const user = userEvent.setup();
+    const viewButton = screen.getByText('View');
+    await user.click(viewButton);
+
+    const url = screen.getByText(blog.url);
+    const likes = screen.queryByText(`likes: ${blog.likes} `);
+    expect(url).toBeDefined();
+    expect(likes).toBeDefined();
+  });
+
+  test('calls the "addLike" prop after clicking like button', async () => {
+    const user = userEvent.setup();
+    const viewButton = screen.getByText('View');
+    await user.click(viewButton);
+
+    const likeButton = screen.getByText('like');
+    await user.click(likeButton);
+    await user.click(likeButton);
+    expect(addLike.mock.calls).toHaveLength(2);
   });
 });
