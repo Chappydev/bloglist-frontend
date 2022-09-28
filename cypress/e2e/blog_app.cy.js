9b3f80d09526cf6f8a75db73676fe076321e0a67
Chappydev <danjtchapman@gmail.com>
2022-09-28 10:43:51 -0600

    Complete 5.21

--- cypress/e2e/blog_app.cy.js
+++ cypress/e2e/blog_app.cy.js
@@ -54,18 +54,36 @@ describe('Blog app', function() {
 
       cy.contains('Title... Author...');
     });
+  });
 
-    it('A blog can be liked', function() {
+  describe('When logged in and a blog created', function() {
+    beforeEach(function() {
+      cy.get('#username').type('georgie');
+      cy.get('#password').type('password');
+      cy.get('#login-button').click();
+
       cy.contains('New Note').click();
       cy.get('.title-input').type('Title...');
       cy.get('.author-input').type('Author...');
       cy.get('.url-input').type('Url...');
       cy.get('#submit-note').click();
+    });
 
+    it('A blog can be liked', function() {
       cy.contains('Title... Author...').parent().find('button').click();
       cy.contains('like').click();
-      cy.contains('Title... now has 1 likes')
+      cy.contains('Title... now has 1 likes');
       cy.contains('likes: 1');
+    });
+
+    it('The blog can be deleted by its creator', function() {
+      cy.contains('Title... Author...').parent().find('button').click();
+      cy.contains('Remove').click();
+
+      cy.get('.success')
+        .should('contain', 'Title... was successfully removed')
+        .and('have.css', 'color', 'rgb(0, 128, 0)');
+      cy.should('not.contain', 'Title... Author...');
     });
   });
 });
